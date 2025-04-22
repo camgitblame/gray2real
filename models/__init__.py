59e9ee25fbfae02b784b79c28d09faed37745c5c
camgitblame <116490613+camgitblame@users.noreply.github.com>
2025-04-22 12:27:20 +0100

    Handle missing models

--- models/__init__.py
+++ models/__init__.py
@@ -32,14 +32,16 @@ def find_model_using_name(model_name):
     model_filename = "models." + model_name + "_model"
     modellib = importlib.import_module(model_filename)
     model = None
-    target_model_name = model_name.replace('_', '') + 'model'
+    target_model_name = model_name.replace("_", "") + "model"
     for name, cls in modellib.__dict__.items():
-        if name.lower() == target_model_name.lower() \
-           and issubclass(cls, BaseModel):
+        if name.lower() == target_model_name.lower() and issubclass(cls, BaseModel):
             model = cls
 
     if model is None:
-        print("In %s.py, there should be a subclass of BaseModel with class name that matches %s in lowercase." % (model_filename, target_model_name))
+        print(
+            "In %s.py, there should be a subclass of BaseModel with class name that matches %s in lowercase."
+            % (model_filename, target_model_name)
+        )
         exit(0)
 
     return model
@@ -56,12 +58,12 @@ def create_model(opt):
 
     This function wraps the class CustomDatasetDataLoader.
     This is the main interface between this package and 'train.py'/'test.py'
-
-    Example:
-        >>> from models import create_model
-        >>> model = create_model(opt)
     """
-    model = find_model_using_name(opt.model)
-    instance = model(opt)
-    print("model [%s] was created" % type(instance).__name__)
-    return instance
+    try:
+        model = find_model_using_name(opt.model)
+        instance = model(opt)
+        print(f"[{opt.name}] ✅ Model [{type(instance).__name__}] was created.")
+        return instance
+    except Exception as e:
+        print(f"[{opt.name}] ❌ Failed to create model '{opt.model}': {str(e)}")
+        raise
